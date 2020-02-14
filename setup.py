a6e6f62c61e2cd5334e307580cbc1589f8570630
Mark Baas <mark.baas@gmail.com>
2020-02-14 10:24:06 +0100

    fixes version in setup py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(
-    name='databricks_kernel2',
-    version='1.1',
-    packages=['databricks_kernel2'],
+    name='databricks_kernel',
+    version='0.2',
+    packages=['databricks_kernel'],
     description='Databricks kernel for jupyter',
     long_description=readme,
     author='Mark Baas',
     author_email='mbaas@hso.com',
-    url='https://github.com/hso-nn/databricks_kernel2',
+    url='https://github.com/hso-nn/databricks_kernel',
     install_requires=[
         'jupyter_client', 'IPython', 'ipykernel'
     ],
